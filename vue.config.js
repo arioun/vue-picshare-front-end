dd1fad440890a1f66d3e39c5bb5784ffd2cd4e3b
xuejiawei1 <anotheryx@outlook.com>
2019-04-27 23:40:17 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     devServer: {
         open: true,
-        host: '13.75.71.92',
+        host: '0.0.0.0',
         port: 8080,
         https: false,
         hotOnly: false,
